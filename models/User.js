a9b8c390df42192866a28ab87be0cb5bd5245060
Trey <trey.shearin3@gmail.com>
2024-05-21 20:17:04 -0400

    added code to model index.js file, started building user model file

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const { Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        }
+    }
+)
