80df87883cf4d54dac3c6f9215b80e358762ac5b
Trey <trey.shearin3@gmail.com>
2024-05-21 21:10:18 -0400

    added virtual function to thought model

--- models/Thought.js
+++ models/Thought.js
@@ -29,6 +29,10 @@ const thoughtSchema = new Schema(
     }
 );
 
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
 const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
