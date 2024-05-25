dca8bc94bae1d382e8c506d625d9c286e2b79bc4
Trey <trey.shearin3@gmail.com>
2024-05-25 18:25:47 -0400

    added routes to userRoutes.js file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,3 +6,10 @@ const {
     updateUser,
     deleteUser,
 } = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
