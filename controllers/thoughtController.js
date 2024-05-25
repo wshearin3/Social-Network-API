1cf1d2ef58a839ab22512f22811ef63a45b7bb4d
Trey <trey.shearin3@gmail.com>
2024-05-25 18:33:30 -0400

    added async functions to get all thoughts and a single thought

---
+++ controllers/thoughtController.js
@@ -0,0 +1,31 @@
+const { ObjectId } = require('mongoose').Types;
+
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+                .select('-__v')
+                .populate('thoughts','friends');
+                
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}
