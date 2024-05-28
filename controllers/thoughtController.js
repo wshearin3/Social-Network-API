073c8950b6a5b379978d307db4c8a2ac3b3754c6
Trey <trey.shearin3@gmail.com>
2024-05-28 12:24:50 -0400

    added updateThought and deleteThought functions to thoughtControllers file

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -36,4 +36,33 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    async updateThought(req, res) {
+        try {
+          const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+          );
+    
+          if (!thought) {
+            res.status(404).json({ message: 'No thought with this id!' });
+          }
+    
+          res.json(thought);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
+      async deleteThought(req, res) {
+        try {
+          const thought = await TextDecoderhought.findOneAndDelete({ _id: req.params.thoughtId });
+    
+          if (!thought) {
+            res.status(404).json({ message: 'No thought with that ID' });
+          }
+        res.json({ message: 'Thought deleted!' });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
 }
